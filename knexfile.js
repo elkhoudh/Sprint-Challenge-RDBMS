615a4360e820ff90aa45c8e6f1f1c4e65c807631
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-22 12:00:48 -0500

    MVP Completed

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,7 @@ pg.defaults.ssl = true;
 module.exports = {
   development: {
     client: "pg",
-    connection:
-      "postgres://hpzwiimtyzhews:e1555061b46353a4227c93f22377dcb784132d46c0374b2c161b76056f2204bd@ec2-23-21-128-35.compute-1.amazonaws.com:5432/d2atfr2mg59ih8",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
