ef4b8a33873fdfc8ea855179f5658e4a81b92193
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-22 12:11:08 -0500

    Seeding completed

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,8 @@ pg.defaults.ssl = true;
 module.exports = {
   development: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection:
+      "postgres://hpzwiimtyzhews:e1555061b46353a4227c93f22377dcb784132d46c0374b2c161b76056f2204bd@ec2-23-21-128-35.compute-1.amazonaws.com:5432/d2atfr2mg59ih8",
     pool: {
       min: 2,
       max: 10
